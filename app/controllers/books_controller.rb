a9c7f9157e3eb0234952884153d445b830a8d28a
matusue <jack322437@gmail.com>
2021-06-15 19:41:17 +0000

    {kari}4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_url, notice: 'Book was successfully destroyed.'
+  end
+
+  private
+  
+    def book_params
+    params.require(:book).permit(:title, :body)
+    end
+    
+    def set_book
+      @book = Book.find(params[:id])
+    end
+  
+end
